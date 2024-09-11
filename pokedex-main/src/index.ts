9e953c361555a20efcbe61d72a1c097ddd127114
VitorVieira <vitorhvvieira@gmail.com>
2024-09-11 14:32:44 -0300

    Finalizando atividade com css

--- pokedex-main/src/index.ts
+++ pokedex-main/src/index.ts
@@ -1,19 +1,36 @@
 import express,{Request, Response} from "express";
 import path from "path";
 
+
+//https://pokeapi.co/api/v2/pokemon/{id or name}/
+
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "/views"));
 
 app.get('/', function (request:Request, response: Response) {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=1025')
         .then(function (res) {
             return res.json();
         })
         .then(function (data) {
-            response.render("index", data);
+            response.render("index", {results: data.results});
         });
 });
+
+app.get('/pokemon/:name', function (request: Request, response: Response){
+    const name = request.params.name;
+   
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then(function (res){
+            return res.json();
+        })
+        .then(function (data) {
+            response.render("pokemon", {pokemon: data});
+        })
+
+});
+
 
 
 
